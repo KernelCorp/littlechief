0078d27caa5ff345121a95bb41debc9cef7f3c33
max-konin <mk@kernel-corp.com>
2013-12-23 18:55:39 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Littlechief::Application.config.secret_key_base = '44e7f741e7678c8df5f33d8627c776cceea185dd5501a9be0d08cf06d1e3832aa2e5661b7a55e9cd775c1339caee5cb20f056e16446f44336fabe131ea8f95f5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Littlechief::Application.config.session_store :cookie_store, key: '_littlechief_session'
